45e8a776ce59364c9e85f8df595eff77dd50e870
Babar Baig <BobBaig@outlook.com>
2021-04-30 00:44:40 -0400

    Update Strip_WS_using_RE.py
    merge changes. rename fn to use only lowercase.

--- Strip_WS_using_RE.py
+++ Strip_WS_using_RE.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 #!/usr/bin/env python
 
@@ -24,30 +23,3 @@ def foo():
 
 foo();
 
-=======
-
-#!/usr/bin/env python
-
-# Run command 'tasklist /nh' to get a list of running tasks in Windows.
-# Capture the output from stdout and strip WS (White Space) using RE (Regular
-# Expressions)
-
-import os
-import re                   #Import the std lib re (Regular Expression) module
-
-def foo():
-    try:                    #Open data file data.txt in readonly mode
-#       f = open('C:/Users/Baig-Daily/Documents/05.Learn/Python/blah.txt','r');
-        f = os.popen('tasklist /nh', 'r')
-    except IOError as e:
-        print("File open error: ", e);
-        return;
-    for eachLine in f:      #Read data one line at a time into eachLine[]
-#       print (re.split(r'\s\s+', eachLine.strip()))
-#       print (re.split(r'\s\s+|\t', eachLine.strip()))
-        print (re.split(r'\s\s+|\t', eachLine.strip()))
-    f.close();          #Good practice, but not necessary
-
-foo();
-
->>>>>>> 3a3eeac04cbe757a5a977da746abe95ab991826d
