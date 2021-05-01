2b8e558a7af680586d6dd7011bc0ae00326d197d
Babar Baig <BobBaig@outlook.com>
2021-05-01 01:57:00 -0400

    Clean-up code

--- gm_battleship.py
+++ gm_battleship.py
@@ -27,50 +27,41 @@ def print_board(board1):
     for row in board1:          #Iterate over each row in board[]
         print(" ".join(row))    #Concat all 'O's, separate by a space
 
-def random_row(board1):
-    row_idx_max = len(board1) - 1
-    return random.randint(0, row_idx_max)
-
-def random_col(board1):
-    col_idx_max = len(board1[0]) - 1
-    return random.randint(0, col_idx_max)
-
 def battleship_game():
     board_sz = 5
-    board_idx_max = board_sz-1
-    print("Let's play Battleship!")
+    print("\nLet's play Battleship!  Guess my ship's location to sink it!")
     board = create_board(board_sz)
     print_board(board)
 
-    ship_row = random_row(board)
-    ship_col = random_col(board)
-    print(ship_row, ship_col)
+    ship_row = random.randint(1, board_sz)
+    ship_col = random.randint(1, board_sz)
+    print(f'Ship location: [{ship_row}, {ship_col}]')
     turn_max = 6
+    found = False
 
     for turn in range(turn_max):
+        if found: break
+        print_board(board)
         print(f'Lives remaining {turn_max - turn}')
-        prompt = "Guess Row: [0 - " + str(board_idx_max) + "]\t"
+        prompt = "Guess Row: [1 - " + str(board_sz) + "]\t"
         guess_row = int(input(prompt))
-        prompt = "Guess Col: [0 - " + str(board_idx_max) + "]\t"
+        prompt = "Guess Col: [1 - " + str(board_sz) + "]\t"
         guess_col = int(input(prompt))
+
         if guess_row == ship_row and guess_col == ship_col:
             print("Congratulations! You sunk my battleship!")
-            break
+            found = True
+        elif guess_row < 1 or guess_row > board_sz or guess_col < 1 or guess_col > board_sz:
+            print("Oops, that's not even in the ocean.")
+        elif board[guess_row-1][guess_col-1] == 'X':
+            print("You guessed that one already.")
         else:
-            if (guess_row<0 or guess_row>board_idx_max) or \
-            (guess_col<0 or guess_col>board_idx_max):
-                print("Oops, that's not even in the ocean.")
-            elif board[guess_row][guess_col]=='X':
-                print("You guessed that one already.")
-                turn -= 1
-            else:
-                print("You missed my battleship!")
-                board[guess_row][guess_col] = 'X'
-                print_board(board)
-        # print(turn+1)
+            print("You missed my battleship!")
+            board[guess_row-1][guess_col-1] = 'X'
 
-        if turn+1==turn_max:
-            print("Game Over")
+    if not found:
+        print("Game Over")
+    print(f'Ship location: [{ship_row}, {ship_col}]')
 
 
 def main():
