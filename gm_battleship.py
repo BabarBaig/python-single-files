9005d3e458c60ab16c20a50a7cdcc8d2bb6969b4
Babar Baig <BobBaig@outlook.com>
2021-04-30 23:01:40 -0400

    Reorganize code. Move major code body into a def()

--- gm_battleship.py
+++ gm_battleship.py
@@ -23,6 +23,7 @@ def create_board(sz_board1):
     return board1
 
 def print_board(board1):
+    print()
     for row in board1:          #Iterate over each row in board[]
         print(" ".join(row))    #Concat all 'O's, separate by a space
 
@@ -34,12 +35,11 @@ def random_col(board1):
     col_idx_max = len(board1[0]) - 1
     return random.randint(0, col_idx_max)
 
-def start_main():
+def battleship_game():
     board_sz = 5
     board_idx_max = board_sz-1
-    board_idx_max_str = str(board_idx_max)
-    board = create_board(board_sz)
     print("Let's play Battleship!")
+    board = create_board(board_sz)
     print_board(board)
 
     ship_row = random_row(board)
@@ -48,8 +48,11 @@ def start_main():
     turn_max = 6
 
     for turn in range(turn_max):
-        guess_row = int(input("Guess Row [0-:"+board_idx_max_str+"]:"))
-        guess_col = int(input("Guess Col [0-:"+board_idx_max_str+"]:"))
+        print(f'Lives remaining {turn_max - turn}')
+        prompt = "Guess Row: [0 - " + str(board_idx_max) + "]\t"
+        guess_row = int(input(prompt))
+        prompt = "Guess Col: [0 - " + str(board_idx_max) + "]\t"
+        guess_col = int(input(prompt))
         if guess_row == ship_row and guess_col == ship_col:
             print("Congratulations! You sunk my battleship!")
             break
@@ -59,18 +62,19 @@ def start_main():
                 print("Oops, that's not even in the ocean.")
             elif board[guess_row][guess_col]=='X':
                 print("You guessed that one already.")
+                turn -= 1
             else:
                 print("You missed my battleship!")
                 board[guess_row][guess_col] = 'X'
                 print_board(board)
-        print(turn+1)
+        # print(turn+1)
 
         if turn+1==turn_max:
             print("Game Over")
 
 
 def main():
-    start_main()
+    battleship_game()
 
 
 if __name__ == '__main__':
