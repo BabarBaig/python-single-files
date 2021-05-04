abaccd089ea9b97bb2b78b24a8b5b0574afdcc4e
Babar Baig <BobBaig@outlook.com>
2021-05-03 23:12:34 -0400

    Delete some dup files

--- read_file_contents.py
+++ read_file_contents.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 
 def read_file_contents(base_dir, read_file):
@@ -12,18 +11,3 @@ def read_file_contents(base_dir, read_file):
     print(file_in_memory)
 
 read_file_contents(r'C:\000\01_proj\p_Python_IDLE', 'read_file_contents_data.txt')
-=======
-import os
-
-def read_file_contents(base_dir, read_file):
-    ''' This can be used as a helper function to read contents of a file
-    '''
-    os.chdir(base_dir)
-    # print(os.listdir())
-    file_handle = open(read_file)
-    file_in_memory = file_handle.read()    # Read entire file into memory
-    file_handle.close()
-    print(file_in_memory)
-
-read_file_contents(r'C:\000\01_proj\p_Python_IDLE', 'read_file_contents_data.txt')
->>>>>>> 3a3eeac04cbe757a5a977da746abe95ab991826d
