25114522776577286a4ec581d0a55921b0129307
Babar Baig <BobBaig@outlook.com>
2021-05-03 22:21:59 -0400

    Rename files to use lower case

--- Threads.py
+++ Threads.py
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 #!/usr/bin/env python
-#-------------------------------------------------------------------------------
-# Name:        Concurrent.Futures.py
-# Purpose:
-#
-# Author:      Babar Baig
-#
-# Created:     06/18/2012
-# Copyright:   (c) Baig-Daily 2012
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+
 
 #from os import getcwd
 #from shutil import move
@@ -17,14 +7,15 @@
 #from time import ctime, sleep
 #from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
+import time
 
 def Loop(loop_count):
-    print("Starting loop", loop_count,"at:", ctime())
-    sleep(2)
-    print("Ending   loop", loop_count,"at:", ctime())
+    print("Starting loop", loop_count,"at:", time.ctime())
+    time.sleep(2)
+    print("Ending   loop", loop_count,"at:", time.ctime())
 
 def main():
-    with ThreadPoolExecutor(max_workers=4) as e:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as e:
         future = e.submit(Loop, 1)
         future = e.submit(Loop, 2)
         future = e.submit(Loop, 3)
@@ -33,39 +24,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
-#!/usr/bin/env python
-#-------------------------------------------------------------------------------
-# Name:        Concurrent.Futures.py
-# Purpose:
-#
-# Author:      Babar Baig
-#
-# Created:     06/18/2012
-# Copyright:   (c) Baig-Daily 2012
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
-#from os import getcwd
-#from shutil import move
-#from concurrent.futures import ProcessPoolExecutor
-#from time import ctime, sleep
-#from concurrent.futures import ThreadPoolExecutor
-import concurrent.futures
-
-def Loop(loop_count):
-    print("Starting loop", loop_count,"at:", ctime())
-    sleep(2)
-    print("Ending   loop", loop_count,"at:", ctime())
-
-def main():
-    with ThreadPoolExecutor(max_workers=4) as e:
-        future = e.submit(Loop, 1)
-        future = e.submit(Loop, 2)
-        future = e.submit(Loop, 3)
-        future = e.submit(Loop, 4)
-        print(future.result())
-
-if __name__ == '__main__':
-    main()
->>>>>>> 3a3eeac04cbe757a5a977da746abe95ab991826d
