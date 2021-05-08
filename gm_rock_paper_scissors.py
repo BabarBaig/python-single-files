617668cbc44bfc7725ba7ad15a42888c8d79caa8
Babar Baig <BobBaig@outlook.com>
2021-05-08 00:47:52 -0400

    Implement popular game: rock paper scissors

--- gm_rock_paper_scissors.py
+++ gm_rock_paper_scissors.py
@@ -1,5 +1,32 @@
 import random
+
+
 def play():
-    user = input("Choose [rock  paper  scissors]:\t")
+    user = input("Choose [(r)ock  (p)aper  (s)cissors, (q)uit]:\t")
     computer = random.choice(['r', 'p', 's'])
-    
+
+    if user == 'q':
+        return user
+
+    if user == computer:
+        return "It's a tie"
+
+    # r > s, s > p, p > r
+    if is_win(user, computer):
+        return 'Computer chose [' + computer + '].  You won!'
+
+    return 'Computer chose [' + computer + '].  You lost!'
+
+
+def is_win(player, opponent):
+    """ r > s, s > p, p > r
+    return true if player wins """
+
+    if player == 'r' and opponent == 's':   return True
+    if player == 's' and opponent == 'p':   return True
+    if player == 'p' and opponent == 'r':   return True
+    return False
+
+
+while (retval := play()) != 'q':
+    print(retval)
