463963851c926c625d4b66d159c948a65c906286
Babar Baig <BobBaig@outlook.com>
2021-06-07 03:28:49 -0400

    Start building class Algo

--- algo/algo_mail.py
+++ algo/algo_mail.py
@@ -1,1 +1,13 @@
-print('Hello, world!')
+import sys
+
+class Algo():
+    def __init__(self):
+        pass
+
+    def run(self):
+        print('Hello, world!')
+
+
+if __name__ == '__main__':
+    algo = Algo()
+    algo.run()
