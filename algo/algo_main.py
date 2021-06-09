7d9b42e7b443e3ba18e25d72b55d7fbd0adf2e8a
Babar Baig <BobBaig@outlook.com>
2021-06-09 00:59:19 -0400

    Fleshing-out the rules ...

--- algo/algo_main.py
+++ algo/algo_main.py
@@ -6,7 +6,8 @@ class Counter():
     def __init__(self, start=0):
         self.counter = start
 
-    def get_count(self):
+    def get(self):
+        """ Return next value of counter """
         self.counter += 1
         return self.counter
 
@@ -18,68 +19,66 @@ class Algo():
     def __init__(self):
         self.count = Counter()
 
-    def prompt_user(self, prompt: str):
-        resp = input(prompt)
-        if resp == 'q':     sys.exit("Quitting\n")
-        return resp
-
     def __once(self):
         """ Call once per session """
         self.count.reset()
         print()
-        print(f"[{self.count.get_count()}] Check today's biggest gainers & decliners")
-        print(f"[{self.count.get_count()}] Bias against buying what I bought < week ago.")
+        print(f"[{self.count.get()}] Run MStar mo3 ETF Quickrank [https://www.morningstar.com/etfs/screener-rank]")
+        print(f"[{self.count.get()}] Check today's biggest gainers & decliners")
+        print(f"[{self.count.get()}] Bias against buying what I bought < week ago.")
 
     def __all(self):
         """ Suggested actions common to all accounts """
-        print(f'[{self.count.get_count()}] Check open orders')
-        print(f'[{self.count.get_count()}] Check cash')
-        print(f'[{self.count.get_count()}] Check ModAlloc')
+        print(f'[{self.count.get()}] Check open orders.  Push BUYs')
+        print(f'[{self.count.get()}] Check cash')
+        print(f'[{self.count.get()}] Check ModAlloc')
 
     def ind441(self):
-        print('In Ind441')
+        print('[Ind441] *********')
         self.__all()
-        print(f"[{self.count.get_count()}] MaxBal > $15k ? Trim.")
-        print(f"[{self.count.get_count()}] Check today's biggest decliners and gainers")
+        print(f"[{self.count.get()}] MaxBal > $15k ? Trim.")
+        print(f"[{self.count.get()}] Check today's biggest decliners and gainers")
 
     def roth089(self):
-        print('In Roth089')
+        print('[Roth089] *********')
         self.__all()
 
     def roll128(self):
-        print('In Roll128')
+        print('[Roll128] *********')
         self.__all()
 
     def utma611(self):
-        print('In UTMA611')
+        print('[UTMA611] *********')
         self.__all()
 
     def jroll124(self):
-        print('In JRoll124')
+        print('[JRoll124] *********')
         self.__all()
 
     def jroth900(self):
-        print('In JRoth900')
+        print('[JRoth900] *********')
         self.__all()
-        print(f"[{self.count.get_count()}] MaxBal > $4k ? Trim.")
+        print(f"[{self.count.get()}] MaxBal > $4k ? Trim.")
 
     def jind668(self):
-        print('In JInd668')
+        print('[Ind668] *********')
         self.__all()
-        print(f"[{self.count.get_count()}] Check today's biggest decliners and gainers")
+        print(f"[{self.count.get()}] Check today's biggest decliners and gainers")
 
     def utma118(self):
-        print('In JRoth900')
+        print('[JRoth900] *********')
         self.__all()
-        print(f"[{self.count.get_count()}] MaxBal > $1.5k ? Trim.")
+        print(f"[{self.count.get()}] MaxBal > $1.5k ? Trim.")
 
     def run(self):
         """ Create a list of function pointers and call them in a loop """
         self.__once()
+        # List of function pointers below
         accounts = [self.ind441, self.roth089, self.roll128, self.utma611, self.jroll124,
                 self.jroth900, self.jind668, self.utma118]
         for account in accounts:
-            if self.prompt_user(f'\n[{account.__name__}] ********* [(q)uit, (s)kip]\t') == 's':    return
+            # self.prompt_user(f'\n[{account.__name__}] ********* [(q)uit]\t')
+            if input(f'\n[{account.__name__}] ********* [(q)uit]\t') == 'q': sys.exit("Goodbye ...\n")
             self.count.reset()
             account()
         print()
