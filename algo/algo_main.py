5937a329ae368732de29cdaa8cc92cffcf0384e4
Babar Baig <BobBaig@outlook.com>
2021-06-12 16:42:08 -0400

    Ongoing readability changes

--- algo/algo_main.py
+++ algo/algo_main.py
@@ -4,15 +4,16 @@ import sys
 
 class Counter():
     def __init__(self, start=0):
-        self.counter = start
+        self.counter: int = start
 
-    def get(self):
+    def get(self) -> int:
         """ Return next value of counter """
         self.counter += 1
         return self.counter
 
-    def reset(self):
+    def reset(self) -> int:
         self.counter = 0
+        return self.counter
 
 
 class Algo():
@@ -25,22 +26,19 @@ class Algo():
         print()
         # print(f"[{self.count.get()}] HIGHLY DISPURPTIVE IDEA: The higher the balance, the higher " +
                 # "the % gain it should show.  Balance & Gain should be linearly related.")
+        # print(f"[{self.count.get()}] Check today's biggest gainers & decliners")
         print(f"[{self.count.get()}] Run MStar mo3 ETF Quickrank [https://www.morningstar.com/etfs/screener-rank]")
-        print(f"[{self.count.get()}] Stocks can have good/bad entry points.  Loot at y1 chart to decide.")
-        print(f"[{self.count.get()}] Check today's biggest gainers & decliners")
-        print(f"[{self.count.get()}] Bias against buying what I bought < week ago.")
+        print(f"[{self.count.get()}] Stocks can have good/bad entry points.  See y1 chart to decide.")
+        print(f"[{self.count.get()}] Souring on unknown stocks b/c of time and unpredictability.")
+        print(f"[{self.count.get()}] Build large balances ONLY if SD/Return > 1")
+        print(f"[{self.count.get()}] Wait a week before buying more.")
         print(f"[{self.count.get()}] Recent momentum [MOON MRNA SFYF SPXL]")
-
-    # def __all(self):
-    #     """ Suggested actions common to all accounts """
-    #     print(f'[{self.count.get()}] Check open orders.  Push BUYs')
-    #     print(f'[{self.count.get()}] Check cash')
-    #     print(f'[{self.count.get()}] Check ModAlloc')
-    #     print(f'[{self.count.get()}] Consider buying $LoBal')
 
     def ind441(self):
         self.count.reset()
         print('[Ind441] *********')
+        print(f'[{self.count.get()}] Sell SCHP SCHR SPIT VGIT to $1000 > AOM in trades of $2k daily.')
+        print(f'[{self.count.get()}] Cash to $10,000 > Excess cash > AOM in trades of $2k daily.')
         print(f'[{self.count.get()}] Check open orders.  Push BUYs')
         print(f'[{self.count.get()}] Check cash')
         print(f'[{self.count.get()}] Check ModAlloc')
@@ -114,7 +112,7 @@ class Algo():
     def run(self):
         """ Create a list of function pointers and call them in a loop """
         self.__once()
-        # List of function pointers below
+        # List of function pointers:
         accounts = [self.ind441, self.roth089, self.roll128, self.utma611, self.jroll124,
                 self.jroth900, self.jind668, self.utma118]
         for account in accounts:
