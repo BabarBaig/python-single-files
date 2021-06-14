73c2b72ed312b32b1bc6cb3abc544a2813818c02
Babar Baig <BobBaig@outlook.com>
2021-06-13 23:39:08 -0400

    Manual merge changes

--- count_a_vowel.py
+++ count_a_vowel.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 def a(x, y, z):
     if x:
@@ -14,20 +13,3 @@ def b(q, r):
 #print(a(3>2, a, b))
 print(b(a, b))
 
-=======
-
-def a(x, y, z):
-    if x:
-        return y
-    else:
-        return z
-
-def b(q, r):
-    return a(q>r, q, r)
-
-#print(a(False, 2, 3))
-#print(b(3, 2))
-#print(a(3>2, a, b))
-print(b(a, b))
-
->>>>>>> 3a3eeac04cbe757a5a977da746abe95ab991826d
