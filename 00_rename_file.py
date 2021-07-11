6b960e818426e052f868ceb386b024363079f9aa
Babar Baig <BobBaig@outlook.com>
2021-07-11 03:48:13 -0400

    Make code compact

--- 00_rename_file.py
+++ 00_rename_file.py
@@ -2,14 +2,21 @@ import os
 import sys
 
 
+def switch_folder(folder: str) -> None:
+    print(f'Switching to folder [{folder}]')
+    try:
+        os.chdir(folder)
+    except OSError as e:
+        print('Unable to open folder:', e)
+        sys.exit(0)
+
+
 def rename_files_in_cur_dir(str_old: str, str_new: str) -> None:
     dont_prompt: bool = False
-
     for fn_old in os.listdir():
         if str_old not in fn_old:
             continue                # Current file is not a match.  Go to next one.
         fn_new: str = fn_old.replace(str_old, str_new)
-
         if dont_prompt:
             print(f'Rename:\n{fn_old}\n{fn_new}')
         else:
@@ -34,18 +41,10 @@ def rename_files_in_cur_dir(str_old: str, str_new: str) -> None:
             sys.exit(0)
 
 
-def switch_folder(folder: str) -> None:
-    print(f'Switching to folder [{folder}]')
-    try:
-        os.chdir(folder)
-    except OSError as e:
-        print('Unable to open folder:', e)
-        sys.exit(0)
-
-
-def rename_files_main() -> None:
+def rename_files() -> None:
     """ Based on how this Python script is executed, cwd can be C:\\Windows\\System32, or
     C:\\Users\\BabBa, or the correct folder.  Get to folder where target files are located.
+    NOTE: We rename files in current folder and its subfolder before asking for new str_old str_new.
     """
     cwd = os.getcwd()
     resp = input(f'Enter home dir:\n{cwd}\n')
@@ -55,18 +54,14 @@ def rename_files_main() -> None:
     while 1:
         switch_folder(cwd)
 
-        str_old = input('\nEnter old string [Quit]:\t')
-        if str_old == 'q':
+        if (str_old := input('\nEnter old string [Quit]:\t')) == 'q':
             sys.exit(0)
 
-        str_new = input('Enter new string [Quit]:\t')
-        if str_new == 'q':
+        if (str_new := input('Enter new string [Quit]:\t')) == 'q':
             sys.exit(0)
 
         rename_files_in_cur_dir(str_old, str_new)
-
         switch_folder(cwd+'\\000_Seen')
-
         rename_files_in_cur_dir(str_old, str_new)
 
 
@@ -84,5 +79,5 @@ if __name__ == '__main__':
     if resp == 'a':
         y2_y4_digit()
     else:
-        rename_files_main()
+        rename_files()
     print("\n")
